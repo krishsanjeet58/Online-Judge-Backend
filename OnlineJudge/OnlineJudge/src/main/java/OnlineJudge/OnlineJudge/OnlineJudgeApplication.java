9feabda69431608a2c87f722d96201cac30aa91d
krishsanjeet58 <choudarysanjeet58@gmail.com>
2025-01-22 08:57:10 +0530

    this is my first commit

---
+++ OnlineJudge/OnlineJudge/src/main/java/OnlineJudge/OnlineJudge/OnlineJudgeApplication.java
@@ -0,0 +1,14 @@
+package OnlineJudge.OnlineJudge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineJudgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineJudgeApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
